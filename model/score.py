6c91c57d168fcb1b01cdc20a0c363e6bdef55854
vaskers5 <danil.krokodil@mail.ru>
2021-02-04 14:13:40 +0300

    Add model and criteration

---
+++ model/score.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        bce_weight = 0.5
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        loss_final = BCE * bce_weight + dice_loss * (1 - bce_weight)
+        return loss_final
+    
+    
+
+class IoU(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IoU, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + smooth)/(union + smooth)
+                
+        return IoU * 100
+    
+class DiceScore(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceScore, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_score = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth) 
+        return dice_score
